b8f080436c68c384583ca7a4b40399efb3ad9629
Kanika <kanikachander1@gmail.com>
2018-07-23 18:09:53 +0530

    travis

--- AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
+++ AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
@@ -10,13 +10,11 @@ namespace AggregateGDPPopulation.Tests
         [Fact]
         public void Test1()
         {
-            //string expectedOutput = Class1.Aggregate();
             StreamReader streamReader1 = new StreamReader(@"../../../../AggregateGDPPopulation.Tests/expected-output.json");
             StreamReader streamReader2 = new StreamReader(@"../../../../AggregateGDPPopulation/output/output.json");
             string actualOutput = streamReader1.ReadToEnd();
             string expectedOutput = streamReader2.ReadToEnd();
             Assert.Equal(expectedOutput, actualOutput);
-
         }
     }
 }
